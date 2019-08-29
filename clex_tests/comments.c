5bbd8cd4bab55567a36cfac132ceaebf0dcb2291
Esteban <estebangsan13@gmail.com>
2019-08-29 17:51:12 -0500

    Agragada carpeta para tests del lexer

---
+++ clex_tests/comments.c
@@ -0,0 +1,14 @@
+/* This is a C-style comment. */
+This is not a comment.
+/* This is another
+ * C-style comment.
+ */
+"This is /* also not a comment */"
+
+/*
+* Some text here
+*/
+
+/**/ Incorrectly removed /**/
+
+/* comentario */ sin terminar */
