13ef8fed40a7a95f5bb595d75fd13d4cf1086312
Esteban <estebangsan13@gmail.com>
2019-11-15 09:23:44 -0500

    Código para probar el checker

---
+++ code.c
@@ -0,0 +1,17 @@
+//int a = 0;
+
+int main(void){
+//    int k = 4.5; // error diff type
+//    int a = 3; // error alrd decl
+//    c = 2; // error not decl
+
+//    int b;
+//    b = 2.42; // error not same type
+    a[5] = 2;
+
+//    int i;
+//    i += 1;
+
+
+//    return 4.2; // error return type
+}
