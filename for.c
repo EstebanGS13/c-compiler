1b555587175c4038c63d1fddede0f0fea72fc8a8
Esteban <estebangsan13@gmail.com>
2019-12-02 01:12:03 -0500

    Agregado código para probar el for

---
+++ for.c
@@ -0,0 +1,16 @@
+void main(void){
+    int i;
+    int j;
+    for(i=0, j=0;;){
+        break;
+        while(2>5){
+            j++;
+            for(;;){
+                break;
+            }
+            break;
+        }
+        break;
+    }
+
+}
